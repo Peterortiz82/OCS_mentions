6c4c71aa1efcc652f97e209efefa8daf1ba76c49
Peterortiz82 <peterortiz82@gmail.com>
2015-09-25 19:47:39 -0400

    added some more Bot methods

--- app/models/bot.rb
+++ app/models/bot.rb
@@ -1,21 +1,42 @@
 class Bot < ActiveRecord::Base
 
   def self.ocs_mentions
-    CLIENT.search("#orlandocitysoccer", result_type: "recent").take(5).each do |t|
+    CLIENT.search("#orlandocitysoccer", result_type: "recent").take(1).each do |t|
       User.create(name: t.user.screen_name, tweet_id: t.id.to_s)
       CLIENT.retweet(t)
     end
   end
 
   def self.orlando_soccer
-    CLIENT.search("Orlando soccer", result_type: "recent").take(5).each do |t|
+    CLIENT.search("Orlando soccer", result_type: "recent").take(1).each do |t|
       User.create(name: t.user.screen_name, tweet_id: t.id.to_s)
       CLIENT.retweet(t)
     end
   end
 
   def self.make_it_count
-    CLIENT.search("Orlando #makeItCount", result_type: "recent").take(5).each do |t|
+    CLIENT.search("Orlando #makeItCount", result_type: "recent").take(1).each do |t|
+      User.create(name: t.user.screen_name, tweet_id: t.id.to_s)
+      CLIENT.retweet(t)
+    end
+  end
+
+  def self.orlando_city_soccer
+    CLIENT.search("Orlando City Soccer", result_type: "recent").take(1).each do |t|
+      User.create(name: t.user.screen_name, tweet_id: t.id.to_s)
+      CLIENT.retweet(t)
+    end
+  end
+
+  def self.orlando_city_goal
+    CLIENT.search("Orlando city Goal", result_type: "recent").take(1).each do |t|
+      User.create(name: t.user.screen_name, tweet_id: t.id.to_s)
+      CLIENT.retweet(t)
+    end
+  end
+
+  def self.orlando_mls
+    CLIENT.search("Orlando MLS", result_type: "recent").take(1).each do |t|
       User.create(name: t.user.screen_name, tweet_id: t.id.to_s)
       CLIENT.retweet(t)
     end
--- lib/tasks/scheduler.rake
+++ lib/tasks/scheduler.rake
@@ -3,5 +3,8 @@ task :update_feed => :environment do
   Bot.ocs_mentions
   Bot.orlando_soccer
   Bot.make_it_count
+  Bot.orlando_city_soccer
+  Bot.orlando_city_goal
+  Bot.orlando_mls
 end
 
